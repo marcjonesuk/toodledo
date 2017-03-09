0422960a5a47d628dbe3aa0fef2b4050fed2f3a9
censorjam <jones.marc@gmail.com>
2017-03-09 07:17:42 +0000

    Adding local repository

---
+++ Web/src/Data/Db.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Update
+    {
+        public string Field { get; set; }
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+    }
+
+    public class ChangeSet
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        public DateTime Committed { get; set; }
+        public Update[] Updates { get; set; }
+        public string UserId { get; set; }
+    }
+
+    public class Content
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        public string Type { get; set; }
+
+        public DateTime Created { get; internal set; }
+        public DateTime LastModified { get; internal set; }
+
+        public string UserId { get; internal set; }
+        public string Body { get; set; }
+        public string Title { get; set; }
+        public int Score { get; set; }
+        public string[] ChangeSets { get; set; }
+        public string[] Parents { get; set; }
+        public string[] Children { get; set; }
+        public string[] Tags { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+    public class Db
+    {
+        public static string Uri { get; set; }
+        public static string Key { get; set; }
+        private static DocumentClient client { get; set; }
+
+        static Db()
+        {
+          
+        }
+
+
+        // ADD THIS PART TO YOUR CODE
+        private async Task GetStartedDemo()
+        {
+
+
+
+
+
+        }
+    }
+}
---
+++ Web/src/TestApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+
+                //var l = new LocalRepository();
+                //l.SetAsync(new Content() { Id = "" });
+
+                //var item = new Content() { Id = "test1", Body = "body" };
+
+                //DocumentDBRepository<Content>.Initialize();
+
+                //var posts = DocumentDBRepository<Content>.GetItemsAsync(c => c.UserId == null).Result;
+
+                //DocumentDBRepository<Content>.CreateItemAsync(item).Wait();
+
+                //var dv = new StackOverflowData();
+                //dv.Test();
+
+                //var x = DocumentDBRepository<Content>.GetItemsAsync(a => a.Body == "body").Result;
+
+                //foreach(var i in x)
+                //{
+                //    DocumentDBRepository<Content>.DeleteItemAsync(i.Id);
+                //}
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadLine();
+        }
+    }
+}
